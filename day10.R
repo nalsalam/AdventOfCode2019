c3981c2c5505450697149a05bf7c2e61f80834b9
Nabeel Alsalam <nabeel.alsalam@gmail.com>
2019-12-19 19:28:59 -0500

    Initial attempt at part 1 at home

--- day10.R
+++ day10.R
@@ -1,5 +1,6 @@
 # Day 10
 # Map of asteroids
+library(tidyverse)
 
 asteroid_map <- c("
 .##.#.#....#.#.#..##..#.#.
@@ -29,12 +30,45 @@ asteroid_map <- c("
 .###..#.#..#...###..###.#.
 #..#..#######..#.#..#..#.#
 ") %>%
-str_replace_all("\\n", "" ) %>%
-str_replace_all("\\.", "0") %>%
-str_replace_all("#", "1") %>%
-matrix(nrow = 26, ncol = 26)
+str_replace_all("\\n", "" )
+str_length(asteroid_map) # 676 or 26x26
+astroid_array <-
+  map_lgl(1:676, 
+          function(i) str_sub(asteroid_map, i, i) == "#")
+astroid_matrix <-
+  matrix(astroid_array, nrow = 26, ncol = 26, byrow = TRUE, dimnames = list(1:26, 1:26))
 
-asteroid_map
+astroid_df <-
+  as_tibble(as.table(astroid_matrix), .name_repair = "minimal")
+
+##########
+
+astroid_matrix[26, 26]
+
+gcd <- function(a, b) {
+  if(a == b) {
+    return(a)
+  }
+  else {
+    gcd(min(a, b), max(a, b) - min(a, b))
+  }
+}
+gcd(105, 252)
+
+station <- tibble(x = 0, y = 0)
+asteroid <- tibble(x = 26, y = 13)
+
+# Points on line between station and another location
+f <- function(station = c(x = 0, y = 0), asteroid = c(x = 26, y = 13)) {
+  line <- asteroid - station
+  f <- gcd(line[1], line[2])
+  g <- line / f
+  map(1:(f-1), function(i) {station + g * i})
+}
+blocked <- f()
+transpose(blocked) %>% unlist()
+as_tibble()
+astroid_matrix[blocked[[1]][1], blocked[[1]][2]]
 
 
 str_length(asteroid_map) # 703 including \n
